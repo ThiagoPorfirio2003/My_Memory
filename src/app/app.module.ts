6aeed880e1c9047a55e312f24832cb210bee2c07
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2024-04-30 15:57:48 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"ppsparcial1","appId":"1:352112907829:web:c21ec82668393f516bc37c","storageBucket":"ppsparcial1.appspot.com","locationId":"us-central","apiKey":"AIzaSyBf6yL4DgTYLQhtwfwFU0EVrdofc1NSp00","authDomain":"ppsparcial1.firebaseapp.com","messagingSenderId":"352112907829"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
