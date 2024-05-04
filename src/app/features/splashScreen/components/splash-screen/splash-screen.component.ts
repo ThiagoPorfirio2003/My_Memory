9b6bc75b3cc8ebb39d3e6778be931adea0afcdb8
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2024-05-04 05:05:33 -0300

    Se cambio el color de fondo del primer splashArt y se soluciono el error que provocaba que la app no redirigiera hacia la pagina auth

--- src/app/features/splashScreen/components/splash-screen/splash-screen.component.ts
+++ src/app/features/splashScreen/components/splash-screen/splash-screen.component.ts
@@ -47,7 +47,7 @@ export class SplashScreenComponent
           {
             setTimeout(() => 
             {
-             // this.utilsService.changeRoute('/auth')
+              this.utilsService.changeRoute('/auth')
             }, 3000);
           })
         });
